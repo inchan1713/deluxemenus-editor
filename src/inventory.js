7ff0479b014e7d7069a8412e9d17c296a57cd6ac
inchan1713 <50742548+inchan1713@users.noreply.github.com>
2026-01-08 05:45:13 +0900

    Update inventory.js

--- src/inventory.js
+++ src/inventory.js
@@ -15,7 +15,7 @@ import 'react-dropdown/style.css';
 import "ace-builds/src-noconflict/mode-yaml";
 import "ace-builds/src-noconflict/theme-github";
 
-const LANGLIST = ['russian', 'english', 'chinese'];
+const LANGLIST = ['russian', 'english', 'chinese', 'japanese'];
 
 let LANG = _LANG;
 const _lang = localStorage.getItem('lang') || 'english';
